80db5699083227ff4a1e5b9af03214a90210437c
rovesciata <rovesciatablu@gmail.com>
2019-04-30 14:33:33 +0900

    スクリプト変更

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,16 +6,18 @@ public class Move : MonoBehaviour
 {
 
     public int speed;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
         speed = 5;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(0, 2, 0);
     }
 }
