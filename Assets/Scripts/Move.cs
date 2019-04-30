01aaf2af42c7eafa44d6957bfe94efb8ff281f98
rovesciata <rovesciatablu@gmail.com>
2019-04-30 14:07:16 +0900

    初コミット

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
