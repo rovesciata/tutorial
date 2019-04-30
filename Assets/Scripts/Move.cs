ab78ed1912a56c045428baa11b4b2c0fef52b013
rovesciata <rovesciatablu@gmail.com>
2019-04-30 15:59:16 +0900

    バージョンアップ

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -11,7 +11,7 @@ public class Move : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        speed = 5;
+        speed = 1;
         rb = GetComponent<Rigidbody>();
     }
 
